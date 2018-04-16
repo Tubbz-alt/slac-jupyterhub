9fcb1847abf8cdee3f8a9d8daf7c6e13e5f48f05
Yee-Ting Li <yee379@gmail.com>
2018-04-16 19:50:24 +0000

    add reload

---
+++ reload-node-selectors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+
+K8S_NAMESPACE="default"
+if [ -n "${1}" ]; then
+    K8S_NAMESPACE="${1}"
+fi
+
+kubectl create -n $K8S_NAMESPACE configmap   hub-config \
+        --from-file=config/jupyterhub_config.py \
+        --from-file=config/node-selectors.yaml \
+        --from-file=config/jupyterhub_config.d  -o yaml --dry-run | kubectl replace -f -
+
