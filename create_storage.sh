3dfcb39685d3bae3102853f7a49b382f45ed3db9
Yee-Ting Li <yee379@gmail.com>
2018-04-16 19:51:49 +0000

    local home updates

--- create_storage.sh
+++ create_storage.sh
@@ -2,4 +2,4 @@
 
 sed "s|__HOME_DIR__|/mnt/generic-users|g" kubernetes/pv_generic-user-home.yaml | kubectl -n jupyter create -f -
 sed "s|__HOME_DIR__|/gpfs/slac/lsst/fs1/u/|g" kubernetes/pv_lsst-home.yaml | kubectl -n jupyter create -f -
-
+sed "s|__HOME_DIR__|/home|g" kubernetes/pv_local-home.yaml | kubectl -n jupyter create -f -
