a60368a46341acac055f624c98a911503854761b
Yee-Ting Li <yee379@gmail.com>
2018-06-20 00:44:50 +0000

    read images from config files

--- config/jupyterhub_config.d/20-spawner.py
+++ config/jupyterhub_config.d/20-spawner.py
@@ -10,6 +10,7 @@ from urllib.error import HTTPError
 from kubespawner.objects import make_pod
 from tornado import gen
 import yaml
+import glob
 
 from traitlets import Unicode, Int, Bool, List, Union
 
@@ -42,47 +43,41 @@ class SLACSpawner(kubespawner.KubeSpawner):
         """
     ).tag(config=True)
 
+    images_config_d = Unicode('',
+        help="""
+        Directory of YAML configuration files of images to spawn
+        """
+    )
     
-    def _options_form_default(self):
-        optform = ""
-
-        optform += '<h3>SLAC JupyterLab Images</h3><br/>\n'
-        optform += '<input type="radio" name="kernel_image" value="%s">%s<br>\n' % ('slaclab/slac-jupyterlab', 'SLAC JupyterLab Image')
-        optform += '<input type="radio" name="kernel_image" value="%s">%s<br>\n' % ('slaclab/slac-jupyterlab-gpu', 'SLAC JupyterLab Image (GPU)')
-
-        # Make options form by scanning container repository
-        try:
-            scanner = ScanRepo(host="hub.docker.com",
-                           owner="lsstsqre",
-                           name="jld-lab",
-                           json=True,
-                           )
-            scanner.scan()
-            lnames, ldescs = scanner.extract_image_info()
-            if not lnames or len(lnames) < 2:
-                raise Exception("could not scan images at lsstsqre/jld-lab")
-
-            now = datetime.datetime.utcnow()
-            nowstr = now.ctime()
-            if not now.tzinfo:
-                nowstr += " UTC"
-
-            title = "LSST lsstsqre/jld-lab Images" 
-            optform += "<h3>LSST lsstsqre/jld-lab Images</h3><br/>\n"
-            optform += "updated at %s<br>\n" % nowstr
-            for idx, img in enumerate(lnames):
-                optform += "      "
-                optform += '<input type="radio" name="kernel_image" value="%s">%s<br>\n' % (img, ldescs[idx])
-
-        except (ValueError, HTTPError) as e:
-            self.log.warning("Could scan images from from %s: %s/%s [%s]" %
-                             (host, owner, repo, str(e)))
-
-        return optform
-
     @property
     def options_form(self):
-        return self._options_form_default()
+
+        if not self.images_config_d:
+            self.images_config_d = '/opt/jupyterhub/config/images.d/'
+        self.log.info("Loading Image Selector from %s" % (self.images_config_d,))
+        # open directory
+        conf_files = glob.glob( self.images_config_d + '/*.yaml' )
+        images = []
+        # for each file read yaml
+        for f in conf_files:
+            self.log.info('  scanning file: %s' % (f,))
+            with open(f, 'r') as conf:
+                try:
+                    images.append( yaml.load(conf) )
+                except Exception:
+                    self.log.warn("Could not parse image list %s" % f)
+           
+        optform = ''
+        for section in images:
+            if 'title' in section:
+                optform += '<h3>%s</h3><br/>\n' % section['title']
+            if 'updated' in section:
+                optform += "updated at %s<br>\n" % section['updated']
+            if 'images' in section:
+                for image in section['images']:
+                    optform += '<input type="radio" name="kernel_image" value="%s">%s<br>\n' % (image['image'], image['description'])
+
+        return optform
 
     @gen.coroutine
     def get_pod_manifest(self):
@@ -213,6 +208,12 @@ class SLACSpawner(kubespawner.KubeSpawner):
                     else:
                         spawn_on = item['spawn_on']
                         spec = item['spec']
+                        if 'cpu' in spec:
+                            self.cpu_limit = spec['cpu']
+                            #self.cpu_guarantee = spec['cpu']
+                        if 'memory' in spec:
+                            self.mem_limit = spec['memory']
+                            #self.mem_guarantee = spec['memory']
                         self.log.info(" using spec: %s" % (spec,))
                         break
 
@@ -251,183 +252,10 @@ class SLACSpawner(kubespawner.KubeSpawner):
         return options
 
 
-class ScanRepo(object):
-    """Class to scan repository and create results.
-
-       Based on:
-       https://github.com/shangteus/py-dockerhub/blob/master/dockerhub.py"""
-
-    host = ''
-    path = ''
-    owner = ''
-    name = ''
-    data = {}
-    debug = False
-    json = False
-    insecure = False
-    sort_field = "comp_ts"
-    dailies = 3
-    weeklies = 2
-    releases = 1
-
-    def __init__(self, host='', path='', owner='', name='',
-                 dailies=3, weeklies=2, releases=1,
-                 json=False,
-                 insecure=False, sort_field="", debug=False):
-        if host:
-            self.host = host
-        if path:
-            self.path = path
-        if owner:
-            self.owner = owner
-        if name:
-            self.name = name
-        if dailies:
-            self.dailies = dailies
-        if weeklies:
-            self.weeklies = weeklies
-        if releases:
-            self.releases = releases
-        if json:
-            self.json = json
-        protocol = "https"
-        if insecure:
-            self.insecure = insecure
-            protocol = "http"
-        if sort_field:
-            self.sort_field = sort_field
-        if debug:
-            self.debug = debug
-        if not self.path:
-            self.path = ("/v2/repositories/" + self.owner + "/" +
-                         self.name + "/tags")
-        self.url = protocol + "://" + self.host + self.path
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.close()
-
-    def close(self):
-        """Close the session"""
-        if self._session:
-            self._session.close()
-
-    def extract_image_info(self):
-        """Build image name list and image description list"""
-        cs = []
-        for k in ["daily", "weekly", "release"]:
-            cs.extend(self.data[k])
-        ldescs = []
-        for c in cs:
-            tag = c["name"].split(":")[-1]
-            if tag[0] == "r":
-                rmaj = tag[1:3]
-                rmin = tag[3:]
-                ld = "Release %s.%s" % (rmaj, rmin)
-            elif tag[0] == "w":
-                year = tag[1:5]
-                week = tag[5:]
-                ld = "Weekly %s_%s" % (year, week)
-            elif tag[0] == "d":
-                year = tag[1:5]
-                month = tag[5:7]
-                day = tag[7:]
-                ld = "Daily %s_%s_%s" % (year, month, day)
-            ldescs.append(ld)
-        ls = [self.owner + "/" + self.name + ":" + x["name"] for x in cs]
-        return ls, ldescs
-
-    def report(self):
-        """Print the tag data"""
-        if self.json:
-            print(json.dumps(self.data, sort_keys=True, indent=4))
-        else:
-            ls, ldescs = self.extract_image_info()
-            ldstr = ",".join(ldescs)
-            lstr = ",".join(ls)
-            print("# Environment variables for Jupyter Lab containers")
-            print("LAB_CONTAINER_NAMES=\'%s\'" % lstr)
-            print("LAB_CONTAINER_DESCS=\'%s\'" % ldstr)
-            print("export LAB_CONTAINER_NAMES LAB_CONTAINER_DESCS")
-
-    def get_data(self):
-        """Return the tag data"""
-        return self.data
-
-    def _get_url(self, **kwargs):
-        params = None
-        resp = None
-        url = self.url
-        if kwargs:
-            params = urllib.parse.urlencode(kwargs)
-            url += "?%s" % params
-        headers = {"Accept": "application/json"}
-        req = urllib.request.Request(url, None, headers)
-        resp = urllib.request.urlopen(req)
-        page = resp.read()
-        return page
-
-    def scan(self):
-        url = self.url
-        results = []
-        page = 1
-        while True:
-            try:
-                resp_bytes = self._get_url(page=page)
-            except Exception as e:
-                raise ValueError("Failure retrieving %s: %s [ data: %s ]" %
-                                 (url, str(e),
-                                  str(resp_bytes.decode("utf-8"))))
-            resp_text = resp_bytes.decode("utf-8")
-            try:
-                j = json.loads(resp_text)
-            except ValueError:
-                raise ValueError("Could not decode '%s' -> '%s' as JSON" %
-                                 (url, str(resp_text)))
-            results.extend(j["results"])
-            if "next" not in j or not j["next"]:
-                break
-            page = page + 1
-        self._reduce_results(results)
-
-    def _reduce_results(self, results):
-        sort_field = self.sort_field
-        r_candidates = []
-        w_candidates = []
-        d_candidates = []
-        for res in results:
-            vname = res["name"]
-            fc = vname[0]
-            res["comp_ts"] = self._convert_time(res["last_updated"])
-            if fc == "r":
-                r_candidates.append(res)
-            if fc == "w":
-                w_candidates.append(res)
-            if fc == "d":
-                d_candidates.append(res)
-        r_candidates.sort(key=lambda x: x[sort_field], reverse=True)
-        w_candidates.sort(key=lambda x: x[sort_field], reverse=True)
-        d_candidates.sort(key=lambda x: x[sort_field], reverse=True)
-        r = {}
-        r["daily"] = d_candidates[:self.dailies]
-        r["weekly"] = w_candidates[:self.weeklies]
-        r["release"] = r_candidates[:self.releases]
-        for tp in r:
-            for v in r[tp]:
-                del(v["comp_ts"])
-        self.data = r
-
-    def _convert_time(self, ts):
-        f = '%Y-%m-%dT%H:%M:%S.%f%Z'
-        if ts[-1] == "Z":
-            ts = ts[:-1] + "UTC"
-        return datetime.datetime.strptime(ts, f)
-
 
 c.JupyterHub.spawner_class = SLACSpawner
 c.SLACSpawner.node_selector_config_file = '/opt/jupyterhub/config/node-selectors.yaml'
+c.SLACSpawner.images_config_d = '/opt/jupyterhub/config/images.d/'
 
 c.SLACSpawner.start_timeout = 60
 c.SLACSpawner.http_timeout = 60
--- redeploy.sh
+++ redeploy.sh
@@ -12,7 +12,8 @@ kubectl delete -n $K8S_NAMESPACE configmap hub-config
 kubectl create -n $K8S_NAMESPACE configmap hub-config \
         --from-file=config/jupyterhub_config.py \
         --from-file=config/node-selectors.yaml \
-        --from-file=config/jupyterhub_config.d
+        --from-file=config/jupyterhub_config.d \
+        --from-file=config/images.d
 kubectl delete -n $K8S_NAMESPACE deployment hub
 kubectl create -n $K8S_NAMESPACE -f kubernetes/deployment.yaml
 
